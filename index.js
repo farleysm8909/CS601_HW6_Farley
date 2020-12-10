cea54e1442d358e2ab0c61f5053f9c4fd67f7962
farleysm8909 <farleysm8909@uwec.edu>
2020-12-09 21:48:03 -0600

    commit hw6

---
+++ index.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+  el: "#imageContainer",
+  data: {
+    //section1
+    myImage1: "images/horse.jpg",
+    current1: true,
+    myImage2: "images/child.JPG",
+    current2: false,
+    myImage3: "images/hsgrad.jpg",
+    current3: false,
+    myImage4: "images/uwecgrad.JPG",
+    current4: false,
+    //section2
+    myImage5: "images/lake.JPG",
+    current5: true,
+    myImage6: "images/tower.jpg",
+    current6: false,
+    myImage7: "images/ely.jpg",
+    current7: false,
+    myImage8: "images/sunflower.jpg",
+    current8: false,
+    myImage9: "images/fish.JPG",
+    current9: false,
+    //section3
+    myImage10: "images/baby1.JPG",
+    current10: true,
+    myImage11: "images/baby2.jpg",
+    current11: false,
+    myImage12: "images/baby3.jpg",
+    current12: false,
+    myImage13: "images/baby4.jpg",
+    current13: false,
+    myImage14: "images/baby5.JPG",
+    current14: false,
+    myImage15: "images/baby6.jpg",
+    current15: false
+  }
+})
