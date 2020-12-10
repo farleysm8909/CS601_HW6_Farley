70a02391c7cf90e19dfc70d9233958e038cd4620
farleysm8909 <farleysm8909@uwec.edu>
2020-12-09 21:57:45 -0600

    fixed broken images

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ var app = new Vue({
     //section3
     myImage10: "images/baby1.JPG",
     current10: true,
-    myImage11: "images/baby2.jpg",
+    myImage11: "images/baby2.JPG",
     current11: false,
     myImage12: "images/baby3.jpg",
     current12: false,
     myImage13: "images/baby4.jpg",
     current13: false,
-    myImage14: "images/baby5.JPG",
+    myImage14: "images/baby5.jpg",
     current14: false,
     myImage15: "images/baby6.jpg",
     current15: false
---
+++ readme.txt
@@ -0,0 +1,6 @@
+WEEK 6
+
+How Sam Farley Went Above and Beyond:
+- Added 3 different image sections instead of just one
+- Added headers for each image section
+- Hosted this assignment on github (https://farleysm8909.github.io/CS601_HW6_Farley/)
